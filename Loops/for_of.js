6e9055f11ff83b584173b7403e7d7d96eaa7af6e
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-02-05 00:19:38 +0530

    adding for_in loop file

---
+++ Loops/for_of.js
@@ -0,0 +1,10 @@
+// print the Lenght of Array without using lenght function 
+//(Most asked Question in Interview)
+
+const arr = [10, 20, 30]
+let count = 0
+for(const _ of arr ){
+    console.log(arr)
+    count++
+}
+console.log(count)
