bee81583a37f4905ab453cd03b9ef42d0abd8fac
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-02-04 22:05:42 +0530

    Adding for_loop file

---
+++ Loops/for_loop.js
@@ -0,0 +1,43 @@
+// Question for For Loop 
+
+// Print Number from 1 to 10 
+
+for(let i=1; i<=10;i++){
+    console.log(i)
+}
+
+
+// Print even numbers between 1 and 20
+
+for(let i=1; i<=20;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+    
+
+// Sum of numbers from 1 to 50
+
+let sum = 0
+for(i=1;i<=50;i++){
+    sum =sum +i
+}
+console.log(sum)
+
+
+// Multiplication table of 7
+
+let a = 7
+for(i=1;i<=10;i++){
+    console.log(a * i)
+}
+
+
+//Factorial of a number
+
+let num = 5
+let fact = 1
+for(i=1;i<=num;i++){
+    fact =  fact *i
+}
+console.log(fact)
