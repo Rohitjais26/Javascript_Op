d9084615fa71d23ac71606af1ad47b6dd40f7472
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-02-04 22:02:45 +0530

    Adding new file

---
+++ script.js
@@ -0,0 +1,69 @@
+alert("kuch gadbad hai")
+
+console.log("code is running")
+
+//Using var to see the result
+var a = confirm("pakka karna hai kya")
+if(a){
+   console.log("haa fix hai ")
+}
+else{
+   console.log("nhi karna hai ")
+}
+
+
+let age = 18
+if(age > 20){
+   console.log("he Can't drive ")
+}
+else{
+   console.log("he can drive ")
+}
+
+//Learning Object 
+let o = {
+   name : "Rohit",
+   age : 22
+}
+console.log(o)
+
+//loop function in css//
+
+//for loop //
+/*let a = 1;
+for (let i = 0; i < 100; i++) {
+   console.log(a + i) ;
+
+}*/
+
+
+
+//learning about function //
+/*function nice(name) {
+   console.log("hey " + name + " is smart boy")
+   console.log("hey" + name + "is smart boy")
+}
+nice("rohit")
+
+//To learn about string//
+let a = ("harry");
+console.log(a[0])
+console.log(a[1])
+console.log(a.length)
+
+let real_name = "rohit";
+let friend = "samay";
+
+console.log(`my name is ${real_name} and friend is ${friend}`)
+
+//properties of string//
+let b = "Rohit"
+let d = "rau"
+console.log(b.length)
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+b.slice((1))
+let c = b.concat(d)
+console.log(c)*/
+
+
