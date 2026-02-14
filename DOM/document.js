db8cbb6590af814635089ecbc5ed4e03f0c53e03
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-02-14 15:04:09 +0530

    adding DOM Concepts files

---
+++ DOM/document.js
@@ -0,0 +1,20 @@
+document.body.childNodes
+
+document.body.firstChild
+
+document.body.firstElementChild
+
+document.body.lastElementChild
+
+document.getElementsByClassName("learn")
+
+document.getElementById("rohit")
+
+document.querySelector(".learn").style.backgroundColor="blue" 
+
+document.querySelector("#rohit").style.backgroundColor="red"
+
+document.querySelectorAll(".learn")
+foreach(e =>{
+    e.style.backgroundColor="green"
+})
---
+++ DOM/scriptr.js
@@ -0,0 +1,6 @@
+console.log("rohit")
+
+let boxes = document.body.getElementsByClassName("container")
+console.log("boxes")
+
+boxes[2].color = "red"
