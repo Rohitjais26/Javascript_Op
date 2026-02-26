5473acb57097478873e0bb5be03a946ca22db140
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-02-27 03:17:21 +0530

    adding New files

---
+++ Loops/max_in_array.js
@@ -0,0 +1,11 @@
+// Code for largest number in Array
+
+let arr = [3,5,2,9]
+let max = arr[0]
+
+for (let i = 1;i<arr.length;i++){
+    if (arr[i]>max){
+        max = arr[i]
+    }
+}
+console.log(max)
