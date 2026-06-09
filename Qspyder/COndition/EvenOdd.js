77a447089d52d4353dc9080b0f10538a6cbf25b3
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-06-09 10:15:29 +0530

    WAP that check Even or Odd

---
+++ Qspyder/COndition/EvenOdd.js
@@ -0,0 +1,10 @@
+// Write a function that checks wheather a number is even or odd
+function EvenOdd(num){
+    if (num%2==0){
+        return "Even"
+    }
+    else{
+        return "Odd"
+    }
+}
+console.log(EvenOdd(5))
