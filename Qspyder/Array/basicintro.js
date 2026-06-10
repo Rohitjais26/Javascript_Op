5e608b1572b795326730efbd31562248be06eee2
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-06-10 09:27:07 +0530

    Learning the Basics Of Javascript

---
+++ Qspyder/Array/basicintro.js
@@ -0,0 +1,25 @@
+// Basics of Array
+
+// How to create an Array
+let arr = [10,20,30]
+console.log(arr)
+
+// Different Data Types in Array
+let arr2 = [10,"Hello", true]
+console.log(arr2)
+
+// Accessing the element of Array
+console.log(arr[0]) // 10
+console.log(arr[1]) // 20
+console.log(arr[2]) // 30
+
+// Array is a object in JavaScript
+console.log(typeof arr) // object
+
+// Length of Array
+console.log(arr.length) // 3
+
+// nested Array
+let arr4= [10, "Ram", ["Learning", "JavaScript"]]
+console.log(arr4[2][0]) // Learning
+
