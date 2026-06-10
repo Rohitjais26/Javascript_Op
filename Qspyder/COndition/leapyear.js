3a214197df80739f4128db06d846151dcc0706fe
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-06-10 09:09:18 +0530

    WAP to check leap year

--- Qspyder/COndition/leapyear.js
+++ Qspyder/COndition/leapyear.js
@@ -1,4 +1,11 @@
+// WAP to check whether a year is leap year or not
 function LeapY(year){
-    if(year%4==0 && )
+    if(year%4==0 && year%100!=0 || year%400==0){
+        return "Leap Year"
+    }
+    else{
+        return "Not Leap Year"
+    }
 
 }
+console.log(LeapY(2020))
