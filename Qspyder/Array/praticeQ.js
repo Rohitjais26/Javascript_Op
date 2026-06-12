f54cc31421ce8f377512e8c9d8eb1787cad70f4f
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-06-12 09:20:33 +0530

    practising the javascript Questions

--- Qspyder/Array/praticeQ.js
+++ Qspyder/Array/praticeQ.js
@@ -33,7 +33,7 @@ function findGreatest(num1,num2){
         return num2 + " is greater than " + num1
     }
 }
-console.log.log(findGreatest(10,20)) 
+console.log(findGreatest(10,20)) 
 
 //Check wheather the number is even or odd
 function EvenOdd(num){
@@ -72,3 +72,29 @@ function Largest(num1,num2,num3){
 }
 console.log(Largest(10,20,30))
 
+//check wheather the character is a vowel or consonant
+function vowelConsonant(char){
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'){
+        return char + " is a vowel"
+    }
+    else{
+        return char + " is a consonant"
+        
+    }
+}
+console.log(vowelConsonant('a')) 
+
+//give grades based on marks (A,B,C,D)
+function grades(marks){
+    if (marks>=90){
+        return "Grade A"
+    }
+    else if (marks>=80){
+        return "Grade B"
+    }
+    else if (marks>=    70){
+        return "Grade C"
+    }           
+
+}
+console.log(grades(90))
