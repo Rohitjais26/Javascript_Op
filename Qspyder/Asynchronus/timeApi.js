14263f09d73cc27da2a2e783f87a50c3c1cbb4e3
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-06-18 09:26:16 +0530

    Executing the Tasks of  Asynshronus javascript

--- Qspyder/Asynchronus/timeApi.js
+++ Qspyder/Asynchronus/timeApi.js
@@ -1,29 +1,66 @@
-// To
+// // To understand the Asynchronus in JavaScript we have to understand the concept of Event Loop and Call Stack.
 
-console.log("1 Line")
-setTimeout(()=>{
-    console.log("I will Exacute After 2 second")
+// console.log("1 Line")
+// setTimeout(()=>{
+//     console.log("I will Exacute After 2 second")
+
+// },2000)
+
+// console.log("2 line")
+
+// // TO learn about setInterval and clearInterval we have to understand the concept of Asynchronus in JavaScript.
+
+// setInterval(() => {
+//     console.log("I will execute agian and agian")
+// },2000);
+
+
+// let count = 0;
+
+// let timer = setInterval(()=>{
+//     count++;
+//     console.log(count)
+
+//     if(count>=5){
+//         clearInterval(timer)
+//         console.log("execution completed")
+//     }
+
+// },3000)
+
+
+
+// tasks
+// 1. Print hello world after 3 second
+
+// setTimeout(()=>{
+//     console.log("Hello World")
+// },3000)
+
+// Countdown from 10 to 1
+
+let count =10;
+let timer = setInterval(()=>{
+    console.log(count)
+    count--;
+
+    if (count<1){
+        clearInterval(timer)
+        console.log("execution completed")
+    
+    }
 
 },2000)
 
-console.log("2 line")
+// OTP expiry timeout
+let countdown = 15
 
-//
+let otpTimer = setInterval(()=>{
+    console.log(countdown)
+    countdown--;
 
-setInterval(() => {
-    console.log("I will execute agian and agian")
-},2000);
-
-
-let count = 0;
-
-let timer = setInterval(()=>{
-    count++;
-    console.log(count)
-
-    if(count>=5){
-        clearInterval(timer)
-        console.log("execution completed")
+    if (countdown<1){
+        clearInterval(otpTimer)
+        console.log("OTP Expired")
     }
-
-},3000)
+},1000)
