45fa7bfbfb7237627128d96a3b92b227e482abf1
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-06-23 09:49:08 +0530

    Learning EventListener Concept

--- Qspyder/DOM/Event.js
+++ Qspyder/DOM/Event.js
@@ -7,12 +7,20 @@
 
 // Function for Event
 
-let delbtn = document.getElementById('btn')
+// let delbtn = document.getElementById('btn')
 
-// document.getElementById('btn').addEventListener('click', ()=>{})
-delbtn.addEventListener('click', ()=>{
-    confirm("Are you sure you want to delete this file")
-    delbtn.style.backgroundColor = "red"
+// // document.getElementById('btn').addEventListener('click', ()=>{})
+// delbtn.addEventListener('click', ()=>{
+//     confirm("Are you sure you want to delete this file")
+//     delbtn.style.backgroundColor = "red"
     
+// })
+
+document.getElementById('show').addEventListener('click',()=>{
+    document.getElementById('demo').style.color = "red"
 })
 
+document.getElementById('update').addEventListener('click',()=>{
+    document.getElementById('info').style.accentColor = "green"
+
+})
